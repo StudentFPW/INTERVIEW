976e9c43e6e32053a6380369c9f8c822e1072d51
LeoN.git.user <jofeleonids00@gmail.com>
2023-06-09 21:33:57 +0300

    09/06/2023

--- project/project/testapp/views.py
+++ project/project/testapp/views.py
@@ -1,31 +1,37 @@
 from django.shortcuts import render, redirect
 from .forms import UploadFileForm
 from .models import *
+import string
 
 
 def upload_file(request):
     wordcount = request.POST.get("wordcount")
     action = request.POST.get("action")
-    word_cnt = 0
     file = Document.objects.all()
+    characters = string.punctuation
+    word_cnt = 0
 
     if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            if file:
-                direct = Document.objects.filter(id=Document.objects.all().values().last()["id"]).values().last()["docfile"]
-                with open(f"media/{direct}", encoding="utf-8") as file:
-                    for word in file:
-                        if word.replace("\n", "") == wordcount:
-                            word_cnt += 1
     else:
         form = UploadFileForm()
 
+    if file:
+        direct = file.values().last()["docfile"]
+        with open(f"media/{direct}", encoding="utf-8") as file:
+            for words in file:
+                for word in words.split():
+                    if word in characters:
+                        continue
+                    if word.lower() == wordcount:
+                        word_cnt += 1
+
     if action == "delete":
         if file:
-            file.filter(id=Document.objects.all().values().last()["id"]).delete()
+            Document.objects.all().last().delete()
         else:
-            return redirect("upload")
+            return render(request, "empty.html")
 
     return render(request, "upload.html", {"form": form, "word_cnt": word_cnt})
